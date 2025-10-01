4c065d289e440e80dd94f19c7b9fc5addd6205e0
Devjb001 <suleelijah13@gmail.com>
2025-10-01 16:38:04 +0100

    remove unwanted file

--- frontend/src/socket.js
+++
@@ -1,9 +0,0 @@
-// src/socket.js
-import { io } from "socket.io-client";
-
-// connect to your backend server
-const socket = io("http://localhost:4000", {
-  transports: ["websocket"], // ensure low-latency connection
-});
-
-export default socket;
