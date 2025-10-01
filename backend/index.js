af13b202aae5ad836336377c8fb0f9fb106b0b8f
Devjb001 <suleelijah13@gmail.com>
2025-10-01 15:52:09 +0100

    im tired

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
   'http://127.0.0.1:5173',
-  'https://your-frontend-app.vercel.app',  
+  'https://guessing-game-one-theta.vercel.app',  
   
 ];
 
